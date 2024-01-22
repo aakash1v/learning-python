dd6945bfb851a3ef121236e66e65c53b3e808b70
Aakash Kumar <aakashkumar113459@gmail.com>
2024-01-23 00:26:49 +0530

    adding file 8

---
+++ Collections/8_dictionary_methods.py
@@ -0,0 +1,17 @@
+#Dict data type in python (Dictionary)
+
+mydict = {101:'Rahul',102:'Amit',103:'Nitish'}
+# print(mydict[2])  we can't acces dictionary with index
+
+print(mydict[101])  #Raul
+
+print(type(mydict))
+# for all values 
+print(mydict.values())
+#for all items
+print(mydict.items())
+# for all keys 
+print(mydict.keys())
+
+print(mydict.get(101))
+
