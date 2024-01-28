288b513ab1be5b58d520f056aa8ea93d1ee86890
Aakash Kumar <aakashkumar113450@gmail.com>
2024-01-28 19:17:02 +0530

    learning exception handling..

--- exception_handling/1_test.py
+++ exception_handling/1_test.py
@@ -1,2 +1,28 @@
 #exception handling program ....
 
+
+try:
+    a = int(input('Enter a number '))
+    b = 7/a
+    print(b)
+
+# use specific error is a good practice..
+# u can so many error 
+
+except ZeroDivisionError as e:
+    print('This is wrong input : ',e)
+
+except ValueError as e:
+    print('This is wrong input : ',e)
+
+except Exception as e :
+    # Exception is a super class
+    print('This is a wrong input :',e)
+    
+finally:
+    print('This will be executed always..')
+
+# list =[2,3,4,5,6,7]
+# for i in list:
+    # print(i,end=' ')
+
