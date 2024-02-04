ce24d20d82b9f9c4e9c9eace8695f3df6bb4cfaf
Aakash Kumar <aakashkumar113450@gmail.com>
2024-02-05 00:48:02 +0530

    editing thread class file...

--- multi_threading/04_thread_class.py
+++ multi_threading/04_thread_class.py
@@ -1,17 +1,30 @@
 from threading import *
+import time
+
+
 class MyThread1(Thread):
     def run(self):
-        print('This is my Thread1')
+        for i in range(10):
+            print('This is my Thread1 : ',i)
+            time.sleep(1)
 
 class MyThread2(Thread):
-    def run(self):
-        print("This is myThread2")
+    for i in range(10):
+            print('This is my Thread2: ',i)
+            time.sleep(1)
 
 # calling
 print('Main thread start.....')
+
 t1 = MyThread1()
-t2 = MyThread2gity()
+t2 = MyThread2()
 t1.start()
 t2.start()
+start = time.time()
+
+t1.join()
+t2.join()
+end = time.time()
+print(end-start)
 
 print('\nMain thread End......')
