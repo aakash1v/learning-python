316becfc0ea26d27315ae87ee58f3212c3835e29
Aakash Kumar <aakashkumar113450@gmail.com>
2024-02-10 23:46:54 +0530

    starting tkinter...

---
+++ Gui/01_start.py
@@ -0,0 +1,14 @@
+from tkinter import *
+#obj  -  class
+myroot = Tk()
+myroot.title('hELLO EVERYONE')
+myroot.geometry('500x500')
+myroot.maxsize('500x500')
+myroot.minsize('300x300')
+# myroot.wm_iconbitmap('/home/aakash/Pictures/Wallpapers/p1.jpg')
+
+
+
+
+myroot.mainloop()
+
---
+++ Gui/02_different_option_of_root.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+myroot = Tk()
+myroot.title('Student Login SEction')
+# myroot.wm_iconbitmap("p1.ico")
+myroot.size("500x500")
+
+
+myroot.mainloop()
+
+
